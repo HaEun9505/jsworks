dba4dc4129a60e89eb408cca055e5b60ee832906
HaEun9505 <0104whgkdms@naver.com>
2021-11-16 12:49:36 +0900

    day07

--- day06/js/gallery.js
+++ day06/js/gallery.js
@@ -1,15 +1,16 @@
-//이미지 넘기기
-var num = 1;
-function nextGallery(){ //전역 변수
-    num++;
+//이미지 넘기기 
+var num = 1;  //전역 변수
+function nextGallery(){ //오른쪽 화살표 클릭
+    //var num = 1;
+    num++;  //1증가
     if(num > 7)
         num = 1;
-    document.getElementById("pic").src="../images/img/" + num + ".jpg";
+    document.getElementById("pic").src="../images/img" + num + ".jpg";
 }
 
 function prevGallery(){
     num--;
     if(num < 1)
         num = 7;
-    document.getElementById("pic").src="../images/img/" + num + ".jpg";
+    document.getElementById("pic").src="../images/img" + num + ".jpg";
 }
